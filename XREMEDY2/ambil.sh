b66eba1b8b8023e6b7656d107e7d4d55ec9bea2b
Agung Ahmad Khairudin - 1406543776 <agung.ahmad@badak.cs.ui.ac.id>
2016-12-29 14:23:29 +0700

    XRemedy2

--- XREMEDY2/ambil.sh
+++ XREMEDY2/ambil.sh
@@ -3,9 +3,10 @@
 ##http://unix.stackexchange.com/questions/20573/sed-insert-something-to-the-last-line
 ##http://stackoverflow.com/questions/16414410/delete-empty-lines-using-sed
 ##http://stackoverflow.com/questions/16643288/sed-to-extract-text-between-two-strings
+##http://unix.stackexchange.com/questions/33110/using-sed-to-get-rid-of-characters
 
 ##simpan file html ke format text
-w3m -dump  http://os162.vlsm.org/2016/11/ranking-os162.html | 
+w3m -dump -cols 1000 http://os162.vlsm.org/2016/11/ranking-os162.html | 
 ## extract text di antara kata ZCZC dan NNNN
 sed -n "/ZCZC/,/NNNN/p" | 
 ## delete ZCZC dan NNNN
@@ -16,5 +17,7 @@ sed  "/\[DISK\]/d" |
 sed '1 i\========================================================== START BERKAS' |
 ## insert baris terakhir
 sed -e '$a\============================================================ STOP BERKAS' |
+## delete bracket []
+tr -d '[]' |
 ## hapus baris kosong jika ada jika selesai output disimpan ke ranking.txt
 awk 'NF'  > ranking.txt
