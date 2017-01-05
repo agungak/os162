a7d940ccfe01b7d559fc499a108822acc12428c9
agungak <agungahmadk@gmail.com>
2017-01-05 18:15:50 +0700

    update ambil3.sh

--- XREMEDY2/ambil3.sh
+++ XREMEDY2/ambil3.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-## (c)2016 Agung Ahmad K -- This is free software
-## kode ini hanya dicocokan untuk sesuai format yang berlaku dimana  diawali dengan nomor urut dan dipisahkan dengan spasi ex: 01. [U1-01] http:xxx
-## kode ini membaca semeua isi file RK-* dan mengubah format AA. [UB-CC] menjadi U B CC AA lalu disimpan ke XHASIL.txt
+## (c)2016 Atung Ahmad K -- This is free software
+## kode ini hanya dicocokan untuk sesuai format yang berlaku dimana  diawali dengan nomor urut dan dipisahkan dengan$
+## kode ini membaca semeua isi file RK-* dan mengubah format AA. [UB-CC] menjadi U B CC AA lalu disimpan ke XHASIL.t$
 ## lalu XHASIL.txt di sort secara asc dan disimpan di YHASIL.txt
 ## selanjutnya file YHASIL.txt dimerge apa bila  U X YY * menjadi satu baris. kemudain disimpan di ZHASIL.txt
-
+file="XHASIL.txt"
+if [ -f $file ] ; then
+    rm $file
+fi
 for  f in RK-*.txt; do
   cat $f |
   egrep "\[U.*]" |
-  tr -d '[].' |
+  awk -F "." '{print $1 " " $2}' |
+  grep "-" |
+  tr -d '[]' |
   sed -e "s/\-/ /g" |
-  sed 's/U/U /g' |
+  awk -F "U" '{print $1$2}' |
   awk 'NF' |
-  awk 'NR<=40 {printf "%s %s %.2d %.2d\n",$2,$3,$4,$1}' >> XHASIL.txt
-# awk '{NR = sprintf("%02d",NR); print NR " " $0}' > XHASIL.txt jika tdk ada nomor urutan
-done 
+  awk '{print "U " $0}' |
+  awk 'NR<=40 {printf "%s %s %.2d %.2d\n",$1,$3,$4,$2}' >> XHASIL.txt
+done
 awk  '{ print $0 }' XHASIL.txt | sort > YHASIL.txt
 awk  '{a[$1FS$2FS$3]=a[$1FS$2FS$3]" "$NF } END {for (i in a) print i""a[i]}' YHASIL.txt | sort > ZHASIL.txt
 
